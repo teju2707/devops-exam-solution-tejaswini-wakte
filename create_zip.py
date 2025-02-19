26053bdde64e789f5e5ec55d8279cf7b641fafe5
Tejaswini Wakte <tejaswiniprasadwakte@gmail.com>
2025-02-19 15:39:44 +0530

    Delete create_zip.py

--- create_zip.py
+++
@@ -1,8 +0,0 @@
-import zipfile
-
-def create_zip():
-    with zipfile.ZipFile('lambda_function_payload.zip', 'w') as zipf:
-        zipf.write('lambda_function.py')
-
-if __name__ == "__main__":
-    create_zip()
