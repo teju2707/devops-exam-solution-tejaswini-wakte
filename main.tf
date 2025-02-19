0b0f4a5e1788c4e05bba0af1b3b7210dc004d2a5
Tejaswini Wakte <tejaswiniprasadwakte@gmail.com>
2025-02-19 15:38:40 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,24 +1,11 @@
 resource "aws_subnet" "private" {
   vpc_id            = data.aws_vpc.vpc.id
-  cidr_block        = "10.0.1.0/24"
+  cidr_block        = "10.0.9.0/24"  # Updated CIDR block
   availability_zone = "ap-south-1a"
-
-  tags = {
-    Name = "Private Subnet"
-  }
 }
 
 resource "aws_route_table" "private" {
   vpc_id = data.aws_vpc.vpc.id
-
-  route {
-    cidr_block     = "0.0.0.0/0"
-    nat_gateway_id = data.aws_nat_gateway.nat.id
-  }
-
-  tags = {
-    Name = "Private Route Table"
-  }
 }
 
 resource "aws_route_table_association" "private" {
@@ -28,36 +15,33 @@ resource "aws_route_table_association" "private" {
 
 resource "aws_security_group" "lambda_sg" {
   vpc_id = data.aws_vpc.vpc.id
-
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
   egress {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
-
-  tags = {
-    Name = "Lambda SG"
-  }
 }
 
 resource "aws_lambda_function" "api_invoker" {
-  function_name = "api_invoker"
-  filename      = "lambda_function_payload.zip"
-  handler       = "lambda_function.lambda_handler"
-  runtime       = "python3.8"
-  role          = data.aws_iam_role.lambda.arn
-  memory_size   = 128
-  timeout       = 3
-
+  filename         = "lambda_function_payload.zip"
+  function_name    = "api_invoker"
+  role             = data.aws_iam_role.lambda.arn
+  handler          = "lambda_function.lambda_handler"
+  runtime          = "python3.8"
+  vpc_config {
+    subnet_ids         = [aws_subnet.private.id]
+    security_group_ids = [aws_security_group.lambda_sg.id]
+  }
   environment {
     variables = {
       SUBNET_ID = aws_subnet.private.id
     }
-  }
-
-  vpc_config {
-    security_group_ids = [aws_security_group.lambda_sg.id]
-    subnet_ids         = [aws_subnet.private.id]
   }
 }
