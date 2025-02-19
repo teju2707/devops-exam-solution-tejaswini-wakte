16515681a6f994e164211d93bd1850abcc4887cd
Tejaswini Wakte <tejaswiniprasadwakte@gmail.com>
2025-02-19 13:26:07 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -5,7 +5,7 @@ provider "aws" {
 terraform {
   backend "s3" {
     bucket = "467.devops.candidate.exam"
-    key    = "YourFirstName.YourLastName"
+    key    = "Tejaswini.wakte"
     region = "ap-south-1"
   }
 }
