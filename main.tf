bcfbb4682b27603d7336f12f49fa6a0f8b270a75
Tejaswini Wakte <tejaswiniprasadwakte@gmail.com>
2025-02-19 13:27:05 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,19 +1,3 @@
-provider "aws" {
-  region = "ap-south-1"
-}
-
-data "aws_nat_gateway" "nat" {
-  id = "nat-0a34a8efd5e420945"
-}
-
-data "aws_vpc" "vpc" {
-  id = "vpc-06b326e20d7db55f9"
-}
-
-data "aws_iam_role" "lambda" {
-  name = "DevOps-Candidate-Lambda-Role"
-}
-
 resource "aws_subnet" "private" {
   vpc_id            = data.aws_vpc.vpc.id
   cidr_block        = "10.0.1.0/24"
